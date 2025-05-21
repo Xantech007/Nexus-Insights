cc0e506e39be95e0c2b98c9290f7cee16add5a87
Xantech007 <alexkosi2002@gmail.com>
2025-05-21 13:40:29 +0200

    Connected to new Firebase project: Nexus Insights

--- firebase.config.js
+++ firebase.config.js
@@ -1,25 +1,22 @@
+// firebaseConfig.js or firebase.js (depending on your project structure)
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiE_ulylBuXSrnNnoSEDHejvL5g8NTOSw",
+  authDomain: "nexus-insights-346a7.firebaseapp.com",
+  projectId: "nexus-insights-346a7",
+  storageBucket: "nexus-insights-346a7.appspot.com", // corrected
+  messagingSenderId: "640016887422",
+  appId: "1:640016887422:web:cc1aa75c54871dbaaff22f"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-//emulator suite for localhost testing
-
-export { auth, db, storage };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
