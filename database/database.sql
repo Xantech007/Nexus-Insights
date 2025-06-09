35687e67449395f06fbe0c4a9e128bb8c37fe2a7
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-06-10 01:04:27 +0200

    Add files via upload

--- database/database.sql
+++ database/database.sql
@@ -1,13 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 09, 2025 at 06:50 PM
+-- Server version: 10.6.21-MariaDB-cll-lve-log
+-- PHP Version: 8.3.21
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+02:00";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `nexuvmvy_nexusinsights`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity`
+--
 
 CREATE TABLE `activity` (
   `act_id` int(11) NOT NULL,
@@ -17,12 +35,24 @@ CREATE TABLE `activity` (
   `date_sent` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blockchaintx`
+--
+
 CREATE TABLE `blockchaintx` (
   `btx_id` int(11) NOT NULL,
   `btx_address` text NOT NULL,
   `btx_txid` text NOT NULL,
   `btx_amount` decimal(65,8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `direct_message`
+--
 
 CREATE TABLE `direct_message` (
   `msg_id` int(11) NOT NULL,
@@ -33,11 +63,23 @@ CREATE TABLE `direct_message` (
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `error_log`
+--
+
 CREATE TABLE `error_log` (
   `id` int(11) NOT NULL,
   `error_message` text DEFAULT NULL,
   `error_time` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hp_deposits`
+--
 
 CREATE TABLE `hp_deposits` (
   `id` int(11) NOT NULL,
@@ -45,6 +87,12 @@ CREATE TABLE `hp_deposits` (
   `username` text NOT NULL,
   `amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hp_transactions`
+--
 
 CREATE TABLE `hp_transactions` (
   `id` int(11) NOT NULL,
@@ -55,12 +103,24 @@ CREATE TABLE `hp_transactions` (
   `payment_mode` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hp_withdrawals`
+--
+
 CREATE TABLE `hp_withdrawals` (
   `id` int(11) NOT NULL,
   `payment_type` text NOT NULL,
   `username` text NOT NULL,
   `amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `investment`
+--
 
 CREATE TABLE `investment` (
   `invest_id` int(11) NOT NULL,
@@ -74,6 +134,12 @@ CREATE TABLE `investment` (
   `status` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `investment_plans`
+--
+
 CREATE TABLE `investment_plans` (
   `id` int(11) NOT NULL,
   `name` text NOT NULL,
@@ -82,6 +148,12 @@ CREATE TABLE `investment_plans` (
   `min_invest` int(11) NOT NULL,
   `max_invest` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
 
 CREATE TABLE `news` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -94,6 +166,12 @@ CREATE TABLE `news` (
   `slug` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_methods`
+--
+
 CREATE TABLE `payment_methods` (
   `id` int(11) NOT NULL,
   `name` text NOT NULL,
@@ -101,6 +179,12 @@ CREATE TABLE `payment_methods` (
   `details` text NOT NULL,
   `photo` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
 
 CREATE TABLE `request` (
   `request_id` int(11) NOT NULL,
@@ -113,6 +197,12 @@ CREATE TABLE `request` (
   `payment_info` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
 CREATE TABLE `transaction` (
   `trans_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -122,6 +212,12 @@ CREATE TABLE `transaction` (
   `remark` text NOT NULL,
   `balance` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -144,92 +240,210 @@ CREATE TABLE `users` (
   `date_view` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `visitor_logs`
+--
+
+CREATE TABLE `visitor_logs` (
+  `id` int(11) NOT NULL,
+  `page_name` varchar(255) NOT NULL,
+  `visit_time` datetime NOT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity`
+--
 ALTER TABLE `activity`
   ADD PRIMARY KEY (`act_id`);
 
+--
+-- Indexes for table `blockchaintx`
+--
 ALTER TABLE `blockchaintx`
   ADD PRIMARY KEY (`btx_id`);
 
+--
+-- Indexes for table `direct_message`
+--
 ALTER TABLE `direct_message`
   ADD PRIMARY KEY (`msg_id`);
 
+--
+-- Indexes for table `error_log`
+--
 ALTER TABLE `error_log`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `hp_deposits`
+--
 ALTER TABLE `hp_deposits`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `hp_transactions`
+--
 ALTER TABLE `hp_transactions`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `hp_withdrawals`
+--
 ALTER TABLE `hp_withdrawals`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `investment`
+--
 ALTER TABLE `investment`
   ADD PRIMARY KEY (`invest_id`);
 
+--
+-- Indexes for table `investment_plans`
+--
 ALTER TABLE `investment_plans`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `news`
+--
 ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `payment_methods`
+--
 ALTER TABLE `payment_methods`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `request`
+--
 ALTER TABLE `request`
   ADD PRIMARY KEY (`request_id`);
 
+--
+-- Indexes for table `transaction`
+--
 ALTER TABLE `transaction`
   ADD PRIMARY KEY (`trans_id`);
 
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `uname` (`uname`);
 
+--
+-- Indexes for table `visitor_logs`
+--
+ALTER TABLE `visitor_logs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activity`
+--
 ALTER TABLE `activity`
   MODIFY `act_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `blockchaintx`
+--
 ALTER TABLE `blockchaintx`
   MODIFY `btx_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `direct_message`
+--
 ALTER TABLE `direct_message`
   MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `error_log`
+--
 ALTER TABLE `error_log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `hp_deposits`
+--
 ALTER TABLE `hp_deposits`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `hp_transactions`
+--
 ALTER TABLE `hp_transactions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `hp_withdrawals`
+--
 ALTER TABLE `hp_withdrawals`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `investment`
+--
 ALTER TABLE `investment`
   MODIFY `invest_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `investment_plans`
+--
 ALTER TABLE `investment_plans`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `news`
+--
 ALTER TABLE `news`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `payment_methods`
+--
 ALTER TABLE `payment_methods`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `request`
+--
 ALTER TABLE `request`
   MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `transaction`
+--
 ALTER TABLE `transaction`
   MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `visitor_logs`
+--
+ALTER TABLE `visitor_logs`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
