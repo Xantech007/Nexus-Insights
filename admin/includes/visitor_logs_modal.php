5b963f4e287ea3a2d1676fe6d17580ade1b2f0fb
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-06-10 03:35:22 +0200

    Update visitor_logs_modal.php

--- admin/includes/visitor_logs_modal.php
+++ admin/includes/visitor_logs_modal.php
@@ -1,25 +1,24 @@
-<!-- Delete -->
+<!-- Delete Modal -->
 <div class="modal fade" id="delete">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">×</span></button>
-              <h4 class="modal-title"><b>Deleting Visitor Log</b></h4>
-            </div>
-            <div class="modal-body">
-              <form class="form-horizontal" method="POST" action="delete_visitor_log.php">
-                <input type="hidden" class="did" name="id">
-                <div class="text-center">
-                    <p>DELETE VISITOR LOG</p>
-                    <h2 class="bold name"></h2>
-                </div>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-              <button type="submit" class="btn btn-danger btn-flat" name="delete"><i class="fa fa-trash"></i> Delete</button>
-              </form>
-            </div>
-        </div>
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+        <h4 class="modal-title"><b>Delete Visitor Logs</b></h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal" method="POST" action="delete_visitor_logs.php" id="delete_form">
+          <input type="hidden" class="did" name="id">
+          <input type="hidden" class="visitor-id" name="visitor_id">
+          <p>Are you sure you want to delete all logs for Visitor ID <span class="name"></span>?</p>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+        <button type="submit" class="btn btn-danger btn-flat" form="delete_form"><i class="fa fa-trash"></i> Delete</button>
+      </div>
     </div>
+  </div>
 </div>
