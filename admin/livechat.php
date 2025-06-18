713926379b988ac5c2e6ea3c54c545bdc82bbf7f
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-06-18 15:36:08 +0200

    Update livechat.php

--- admin/livechat.php
+++ admin/livechat.php
@@ -1,7 +1,7 @@
 <?php
-include('../../inc/config.php');
-include('../../admin/includes/format.php');
-include('../inc/session.php');
+include('../inc/config.php');
+include('includes/format.php');
+include('inc/session.php');
 
 $page_name = 'Admin Live Chat';
 $page_parent = '';
