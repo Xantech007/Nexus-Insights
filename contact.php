df44dff8ddda43d80ea0e826e93fbb92ee0e893f
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-06-18 14:49:53 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -45,37 +45,7 @@
       </div>
     </section>
     <!-- inner hero end -->
-
-
-    <!-- contact section start -->
-    <section class="pt-120 pb-120">
-      <div class="container">
-        <div class="contact-wrapper">
-          <div class="row">
-            <div class="col-lg-6 contact-thumb bg_img" data-background="assets/images/bg/bg-1.jpg"></div>
-            <div class="col-lg-6 contact-form-wrapper">
-              <h2 class="font-weight-bold">Contact.</h2>
-              <h2 class="font-weight-bold">Get in touch.</h2>
-              <span>Leave us a message</span>
-              <form class="contact-form mt-4">
-                <div class="form-row">
-                  <div class="form-group col-lg-6">
-                    <input type="text" name="contact-name" placeholder="Full Name" class="form-control">
-                  </div>
-                  <div class="form-group col-lg-6">
-                    <input type="email" name="contact-name" placeholder="Email Address" class="form-control">
-                  </div>
-                  <div class="form-group col-lg-12">
-                    <textarea class="form-control" placeholder="Message"></textarea>
-                  </div>
-                  <div class="col-lg-12">
-                    <button type="submit" class="cmn-btn">Send Message</button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div><!-- contact-wrapper end -->
+      
       </div>
       <div class="container pt-120">
         <div class="row justify-content-center">
