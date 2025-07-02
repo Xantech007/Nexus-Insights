ccbdc815e3d77d48452dee8dad639a1f101a30a4
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-02 03:53:39 +0200

    Update pre-loader.php

--- inc/pre-loader.php
+++ inc/pre-loader.php
@@ -1,6 +1,13 @@
+<div class="preloader" id="preloader">
+  <div class="preloader-container">
+    <span class="animated-preloader"></span>
+  </div>
+</div>
 
-    <div class="preloader">
-      <div class="preloader-container">
-        <span class="animated-preloader"></span>
-      </div>
-    </div>
+<script>
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      document.getElementById('preloader').style.display = 'none';
+    }, 1000); // Hides preloader 1 second after page load
+  });
+</script>
