5a02e3ed35090b078397e9160cb036ab932bd19b
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-02 04:05:38 +0200

    Update header.php

--- account/inc/header.php
+++ account/inc/header.php
@@ -1,5 +1,5 @@
 <?php
-include('../inc/pre-loader.php');
+include('../../inc/pre-loader.php');
 // inc/header.php
 // Ensure $settings, $conn, and $id are available (loaded via config.php or session.php)
 if (!isset($settings)) {
