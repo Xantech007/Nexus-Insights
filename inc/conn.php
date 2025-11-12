1f0495260c6d2b0edf0d73de1a8e7b4ae71e2aba
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-11-12 07:35:14 +0000

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -2,7 +2,7 @@
 class Database {
     private $server = "mysql:host=localhost;dbname=nexuvmvy_nexusinsights;charset=utf8mb4";
     private $username = "nexuvmvy_nexusinsights"; // Updated username
-    private $password = "Xander24427279"; // Updated password
+    private $password = "Xander24427279"; // Updated password 1
     private $options = array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
